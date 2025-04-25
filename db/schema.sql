6aa1d265b221187dbafa433593724459e844ca14
f11v <maria.vasconez.rea@gmail.com>
2025-04-24 22:11:01 -0500

    bd

--- db/schema.sql
+++ db/schema.sql
@@ -16,15 +16,18 @@ CREATE TABLE log_cambios_resultados (
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TRIGGER auditoria_resultados
-AFTER INSERT OR UPDATE ON resultados_examenes
+CREATE TRIGGER auditoria_resultados_insert
+AFTER INSERT ON resultados_examenes
 FOR EACH ROW
 BEGIN
     INSERT INTO log_cambios_resultados (operacion, paciente_id, tipo_examen, fecha)
-    VALUES (
-        CASE WHEN NEW.rowid THEN 'INSERT' ELSE 'UPDATE' END,
-        NEW.paciente_id,
-        NEW.tipo_examen,
-        CURRENT_TIMESTAMP
-    );
+    VALUES ('INSERT', NEW.paciente_id, NEW.tipo_examen, CURRENT_TIMESTAMP);
+END;
+
+CREATE TRIGGER auditoria_resultados_update
+AFTER UPDATE ON resultados_examenes
+FOR EACH ROW
+BEGIN
+    INSERT INTO log_cambios_resultados (operacion, paciente_id, tipo_examen, fecha)
+    VALUES ('UPDATE', NEW.paciente_id, NEW.tipo_examen, CURRENT_TIMESTAMP);
 END;
